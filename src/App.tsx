3e48e234715a2c326a7b05669c2f9a38bb41d445
Otabek1990 <fozilovotabek424@gmail.com>
2026-03-25 18:49:20 +0500

    home va login sahifa yaratildi. routelar ulandi

--- src/App.tsx
+++ src/App.tsx
@@ -1,102 +1,22 @@
-import Card from "./Card";
+import { Route, Routes } from "react-router-dom";
 import Banner from "./components/Banner";
 import Header from "./components/Header";
 import Home from "./pages/home";
-import type { IUser, TMeva } from "./shared/types/types";
+import Login from "./pages/login";
 
-// tsda Role
-// useState,
-// type va interface;
-// admin,operator,seller
-// type Role = "admin" | "operator" | "seller";
-// let role: Role = "operator";
-// console.log(role);
-// type Kurs = "frontend" | "backend";
 
-// interface IHotel {
-//   title: string;
-//   address: {
-//     street: string;
-//     city: string;
-//   };
-//   prices: {
-//     1: number;
-//     2: number;
-//   };
-//   filials: string[];
-// }
-
-// const hotel:IHotel = {
-//   title: "hilton",
-//   address:{
-//     street:"afrosiyob",
-//     city:"toshkent"
-//   },
-//   prices:{
-//     1:100,
-//     2:200
-//   },
-//   filials:["yunusobod"]
-// };
-// console.log(hotel);
-// let kurs: Kurs = "frontend";
-// console.log(kurs);
-
-// interface ITalaba {
-//   ism: string;
-//   yosh: number;
-// }
-// interface User extends ITalaba {
-//   manzil: string;
-//   id: string | number;
-// }
-// let user: User = {
-//   ism: "hhh",
-//   yosh: 12,
-//   manzil: "",
-//   id: 1,
-// };
-// console.log(user);
-
-// function types
 
 function App() {
   // role
-  const user: IUser = {
-    ism: "abdulloh",
-    yosh: 23,
-    friends: ["elyor", "ali"],
-  };
-  const mevalar: TMeva[] = [
-    {
-      nom: "olma",
-      narx: 10000,
-      rang: "qizil",
-      qimmat: true,
-      id: 1,
-    },
-    {
-      nom: "banan",
-      narx: 22000,
-      rang: "sariq",
-      qimmat: false,
-      id: 2,
-    },
-    {
-      nom: "xurmo",
-      narx: 8000,
-      rang: "yashil",
-      qimmat: false,
-      id: 3,
-    },
-  ];
-  console.log(mevalar);
- 
+  
+
   return (
     <>
       <Header id={1} ism={"Ali"} yosh={20} />
-      <Banner />
-     
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 interface HeaderProps {
   ism: string;
   yosh:number;
@@ -8,6 +10,8 @@ function Header({ ism,yosh }: HeaderProps) {
   return (
     <header className="bg-blue-500 py-5 px-16">
       <h1>{ism}-{yosh}</h1>
+      <NavLink to={"/"}>Asosiy</NavLink>
+      <NavLink to={"/login"}>Login</NavLink>
     </header>
   );
 }
