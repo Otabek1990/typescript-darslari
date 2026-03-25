dfdab222addad1a3bb5120e9c8ba664442264fdc
Otabek1990 <fozilovotabek424@gmail.com>
2026-03-25 18:49:53 +0500

    banner removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import Banner from "./components/Banner";
+
 import Header from "./components/Header";
 import Home from "./pages/home";
 import Login from "./pages/login";
